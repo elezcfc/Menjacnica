bda2c0b86599325d1e58b96e4d4b085526861d3b
ElezStefan <elez43513ei@fon.bg.ac.rs>
2015-04-02 22:49:53 +0200

    Dodate toString, Equals, HashCode metode u obe klase

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -30,5 +30,48 @@ public class Menjacnica {
 		this.mesto = mesto;
 	}
 
+	@Override
+	public String toString() {
+		return "Menjacnica [naziv je " + naziv + ", mesto je " + mesto+"]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((mesto == null) ? 0 : mesto.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((valute == null) ? 0 : valute.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (mesto == null) {
+			if (other.mesto != null)
+				return false;
+		} else if (!mesto.equals(other.mesto))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (valute == null) {
+			if (other.valute != null)
+				return false;
+		} else if (!valute.equals(other.valute))
+			return false;
+		return true;
+	}
+
+	
 	
 }
--- src/menjacnica/Valuta.java
+++ src/menjacnica/Valuta.java
@@ -68,5 +68,60 @@ public class Valuta {
 	public void setKupovni(int kupovni) {
 		this.kupovni = kupovni;
 	}
+
+	@Override
+	public String toString() {
+		return "Valuta [naziv je: " + naziv + ", skraceni naziv je " + skraceni_naziv
+				+ ", datum je " + datum + ", prodajni kurs je " + prodajni + ", srednjikurs je:"
+				+ srednji + ", kupovni kurs je: " + kupovni + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + kupovni;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + prodajni;
+		result = prime * result
+				+ ((skraceni_naziv == null) ? 0 : skraceni_naziv.hashCode());
+		result = prime * result + srednji;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (kupovni != other.kupovni)
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (prodajni != other.prodajni)
+			return false;
+		if (skraceni_naziv == null) {
+			if (other.skraceni_naziv != null)
+				return false;
+		} else if (!skraceni_naziv.equals(other.skraceni_naziv))
+			return false;
+		if (srednji != other.srednji)
+			return false;
+		return true;
+	}
+	
 	
 }
